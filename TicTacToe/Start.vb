1e3a657209bcb04f54586cff2bf0ebf728e8b706
LOPIII <78519952+LOPIII@users.noreply.github.com>
2021-05-21 14:11:35 +0800

    mechanics

--- TicTacToe/Start.vb
+++ TicTacToe/Start.vb
@@ -14,6 +14,8 @@
 
     Private Sub Guna2Button3_Click(sender As Object, e As EventArgs) Handles mechaBt.Click
         Me.Hide()
+        mechncs_new.Show()
+
     End Sub
 
     Private Sub aboutBt_Click(sender As Object, e As EventArgs) Handles aboutBt.Click
